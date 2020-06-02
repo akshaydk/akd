3b1e7b943542a28a75c2ceab34a9837a32d5b590
Akshay kumar Dhupam <akshay@Akshays-MacBook-Pro.local>
2020-06-02 16:03:03 +0530

    Added footer code.

--- components/sections/footer.vue
+++ components/sections/footer.vue
@@ -1,3 +1,37 @@
 <template>
-  <h4> This is footer! </h4>
+  <footer>
+    <div class="footer__main">
+      <div class="container">
+        <div class="footer__content">
+          Feel free to drop in a 'Hi' by sending a 
+          <a target="_blank" href="mailto:dhupamakshay.kumar@gmail.com"> mail</a>. 
+          <br>
+          Conect with <a target="_blank" href="https://www.linkedin.com/in/dhupamakshay/"> Akshay Kumar</a>.
+          <br>
+        </div>
+      </div>  
+    </div>
+    <div class="copyright">
+      &copy; 2020 This site was created using Vue.js + Nuxt.  
+    </div>
+
+  </footer>
 </template>
+
+<style>
+  footer {
+        background-color: #030303;
+  }
+  .footer__main {
+    padding: 50px 0;
+  }
+  .footer__content, .copyright {
+    text-align: center;
+    color: whitesmoke;
+  }
+
+  .container{
+    max-width: 1000px;
+  }
+
+</style>
